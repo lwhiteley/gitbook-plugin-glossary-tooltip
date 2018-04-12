64704bfeb66ec2c33d5970f35b7f8405b3e6ffab
Layton Whiteley <lwhiteley@medullan.com>
2018-04-12 07:24:49 -0500

    update

--- book/plugin.js
+++ book/plugin.js
@@ -2,18 +2,29 @@ require([
     'jquery',
     'gitbook',
 ], function ($, gitbook) {
-    var self = self || {};
     var setup = function () {
         var $selectors = $('.glossary-term');
         $selectors.each(function() {
             var $this = this;
+            var title = $this.attr('title');
+            $this.removeAttr('title');
             const instance = new Tooltip($this, {
-                title: $this.attr('title'),
+                title: title,
                 trigger: "hover",
+            },
+            {
+                placement: 'left',
+                modifiers: {
+                    flip: {
+                        behavior: ['left', 'bottom', 'top']
+                    },
+                    preventOverflow: {
+                        boundariesElement: $('div.page-inner')[0],
+                    },
+                },
             });
         })
     };
 
     gitbook.events.bind('page.change', setup);
-    return self;
 });
